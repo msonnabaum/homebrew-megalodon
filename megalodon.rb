3b439ec5834413f52bc19f0fc6d0fdbc0433dcb6
Mark Sonnabaum <mark@sonnabaum.com>
2011-12-07 15:02:53 -0600

    Added homebrew formula for installer script.

---
+++ megalodon.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class Megalodon <Formula
+  head 'git://github.com/msonnabaum/megalodon.git'
+  homepage 'https://github.com/msonnabaum/megalodon'
+
+  def patches; DATA; end
+
+  def install
+    prefix.install Dir['config', 'cookbooks', 'formulas', 'roles']
+    bin.install "megalodon"
+  end
+end
+
+__END__
+ diff --git a/megalodon b/megalodon
+ index 5aaa092..b2c5e10 100755
+ --- a/megalodon
+ +++ b/megalodon
+ @@ -4,7 +4,7 @@ begin
+    require "rubygems"
+    require 'chef'
+  
+ -  cwd = Dir.pwd
+ +  cwd = `brew --prefix megalodon`
+    puts "Copying custom forumulas"
+    system("cp #{cwd}/formulas/* /usr/local/Library/Formula/")
+    puts "Starting chef-solo run"
